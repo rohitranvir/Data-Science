6bac060206747fb38c5879558e569283b82e1537
Rohit <rohitranveer358@gmail.com>
2025-12-21 13:15:43 +0530

    Todays practiced question today i did practice on list , set,  tuple, tuple

---
+++ Python/02_Strings/.ipynb_checkpoints/Slice-operator-checkpoint.py
@@ -0,0 +1,15 @@
+s="abcdefg"
+print(s[0:2:1])
+print(s[0:7:2])
+print(s[:7:1])
+print(s[::])
+print(s[::-1])
+print(s[0::])
+# USing negative index  
+print(s)
+print(s[-1::])
+print(s[::-1])
+print(s[-3:-8:-1])
+print(s[-1::-1])
+print(s[-1:-5:])
+
