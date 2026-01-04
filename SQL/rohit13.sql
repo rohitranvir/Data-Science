b459ba1bbe7f669fe11c64160e0049081373ac71
Rohit <rohitranveer358@gmail.com>
2026-01-04 23:54:24 +0530

    Eda on train  and test dataset

---
+++ SQL/rohit13.sql
@@ -0,0 +1,7 @@
+select * from emp;
+select * from dept;
+--selecting data from both tables by specifying join condition
+select * from emp e,dept d where d.deptno=e.deptno;
+select e.sal,e.ename,d.dname from emp e,dept d; // where d.deptno=e.deptno;
+-- selecting the data from both table by specifying  join condition with filter condition 
+select e.sal,e.job,e.mgr,d.dname from emp e,dept d where e.deptno= d.deptno and d.dname='ACCOUNTING';
