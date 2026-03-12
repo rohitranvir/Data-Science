b77417f9a0434ac4c9e109612eab5a7e43f5c0f5
Rohit Ranvir <rohitranveer358@gmail.com>
2026-03-12 06:08:12 +0530

    Projects , Django ,Javascript

--- Django/astrologyproject/testapp/views.py
+++ Django/astrologyproject/testapp/views.py
@@ -4,5 +4,26 @@ import datetime,random
 def result_view(request):
     msg=[
         'The golden days is ahead',
-        
+        'Better to sleep More in classroom',
+        'Towmarrow will be the Best days in your life',
+        'Towmarrow is the perfect days to purpose to your girlfriend',
+        'Very soon you will get a job',
+
     ]
+    names_list=['katrina','karina','Madhuri','Deepika','Lilly']
+    time=datetime.datetime.now()
+    h=int(time.strftime('%H'))
+    if h < 12:
+        s = 'Good Morning'
+    elif h < 16:
+        s = 'Good Afternoon'
+    elif h < 21:
+        s = 'Good Evening'
+    else:
+        s = 'Good Night'
+    name=random.choice(names_list)
+    msg=random.choice(msg)
+    my_dict={
+        'time':time,'name':name,'msg':msg,'wish':s
+    }
+    return render(request,'testapp/results.html',my_dict)
