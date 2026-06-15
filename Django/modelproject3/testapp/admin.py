056a0b9fa45ec6bfb4b9eb69fe9226e11c190750
rohit-ranvir <rohitranveer358@gmail.com>
2026-06-16 01:28:48 +0530

    DELETED FILES

--- Django/modelproject3/testapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.admin import ModelAdmin
-from testapp.models import Student
-
-
-# Register your models here.
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['rollno','name','dob','marks','email','phonenumber']
-admin.site.register(Student,StudentAdmin)
